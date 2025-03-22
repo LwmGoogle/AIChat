9d2564fd5da96be0ffb3a0ab07856e220abddec1
LwmGoogle <93698378+LwmGoogle@users.noreply.github.com>
2025-03-23 02:26:28 +0800

    Update api_config.php
    删除自己的API Key

--- config/api_config.php
+++ config/api_config.php
@@ -1,9 +1,9 @@
 <?php
 // API Keys Configuration
-define('DEEPSEEK_KEY', 'sk-315677d34a8541a39678c8dccb3b2dcf');
-define('CHATGPT_KEY', 'sk-ZB10HAoGaanGIVWmie1GE8FNBRU0vI1JCVWDut9RbZihwgfe');
-define('TONGYI_KEY', 'sk-393b2d807b934a02ade3ff682a67eef4');
-define('GROK_KEY', 'xai-esVvibqRlFQGSfkzpAwowrC4BPxK3ljhCMFGqe1q8qlcXVko03ZeXvyU3otnkdSlSxRM6IgX4GE7YDqO');
+define('DEEPSEEK_KEY', '');
+define('CHATGPT_KEY', '');
+define('TONGYI_KEY', '');
+define('GROK_KEY', '');
 
 // API Endpoints
 define('CHATGPT_API', 'https://api.chatanywhere.tech/v1/chat/completions');
